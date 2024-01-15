395b17f670e108aeb3a2b1a7f9dc58d8e1199060
Vlone Me <francisntima@gmail.com>
2024-01-15 20:58:26 +0000

    Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,24 +3,28 @@
 
 from typing import Tuple, List
 
+
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     Zooms in on an array by duplicating each element 'factor' times.
 
     Parameters:
     - lst (Tuple): The input tuple to be zoomed.
-    - factor (int): The factor by which each element should be duplicated. Defaults to 2.
+    - factor (int): The factor by which each element
+    should be duplicated. Defaults to 2.
 
     Returns:
-    - List: The zoomed-in list containing each element duplicated 'factor' times.
+    - List: The zoomed-in list containing each element
+    duplicated 'factor' times.
     """
-    
+
     """ Creating a list comprehension to zoom in on the input tuple """
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
+
 
 """ Input array """
 array = (12, 72, 91)
