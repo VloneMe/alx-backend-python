5726947240bcdd938570e58f439dc5936583b506
Vlone Me <francisntima@gmail.com>
2024-01-15 20:16:03 +0000

    Duck typing - first element of a sequence

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,6 +5,17 @@ from typing import Union, Any, Sequence
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    This returns the first element of a list if it exists,
+    otherwise returns None.
+
+    Parameters:
+    - lst (list): The input list.
+
+    Returns:
+    Optional[Any]: The first element of the list if it exists,
+    otherwise None.
+    """
     if lst:
         return lst[0]
     else:
