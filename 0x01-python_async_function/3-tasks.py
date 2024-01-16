0d487addcce0a800c28095643b5cf66aaa8cb556
Vlone Me <francisntima@gmail.com>
2024-01-16 02:54:58 +0000

    task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -8,9 +8,11 @@ from typing import List
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
-    Regular function that takes an integer max_delay and returns an asyncio.Task.
+    This regular function that takes an integer max_delay
+    and returns an asyncio.Task.
 
     Args:
         max_delay (int): Maximum delay in seconds.
