d3297d76dc67cdc7bc9f3705603eae6262947aac
Vlone Me <francisntima@gmail.com>
2024-01-16 03:06:57 +0000

    task 2

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,7 +9,8 @@ task_wait_random = __import__('3-tasks').wait_random
 
 async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
-    Asynchronous coroutine that spawns task_wait_random n times with the specified max_delay.
+    Asynchronous coroutine that spawns task_wait_random n times
+    with the specified max_delay.
 
     Args:
         n (int): Number of times to spawn task_wait_random.
