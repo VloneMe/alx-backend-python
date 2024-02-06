656de84ea4bab3a821a3f14438e3ae92c841669d
Vlone Me <francisntima@gmail.com>
2024-02-06 00:53:05 +0000

    All the tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,7 @@ from unittest.mock import patch
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
 
+
 class TestAccessNestedMap(unittest.TestCase):
     """Test cases for the access_nested_map function."""
 
@@ -22,7 +23,8 @@ class TestAccessNestedMap(unittest.TestCase):
         - path (tuple): The path to the desired value within the nested_map.
         - expected_result: The expected result when accessing the nested_map.
 
-        The function should retrieve values from a nested map based on the provided path.
+        The function should retrieve values from
+        a nested map based on the provided path.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
@@ -36,13 +38,16 @@ class TestAccessNestedMap(unittest.TestCase):
 
         Parameters:
         - nested_map (dict): The nested dictionary to traverse.
-        - path (tuple): The path to a key that does not exist in the nested_map.
+        - path (tuple): The path to
+        a key that does not exist in the nested_map.
 
-        The function should raise a KeyError when attempting to access a non-existing key.
+        The function should raise a KeyError when
+        attempting to access a non-existing key.
         """
         with self.assertRaises(KeyError) as error:
             access_nested_map(nested_map, path)
         self.assertEqual(error.exception.args[0], path[-1])
+
 
 class TestGetJson(unittest.TestCase):
     """Test cases for the get_json function."""
@@ -58,7 +63,8 @@ class TestGetJson(unittest.TestCase):
 
         Parameters:
         - test_url (str): The URL for the HTTP request.
-        - test_payload (dict): The expected payload returned by the mocked HTTP call.
+        - test_payload (dict): The expected payload
+          returned by the mocked HTTP call.
         - mock_get_json (MagicMock): The mocked get_json function.
 
         The function should retrieve JSON data from the specified URL.
@@ -67,6 +73,7 @@ class TestGetJson(unittest.TestCase):
         result = get_json(test_url)
         self.assertEqual(result, test_payload)
 
+
 class TestMemoize(unittest.TestCase):
     """Test case for the memoize decorator."""
 
@@ -74,7 +81,8 @@ class TestMemoize(unittest.TestCase):
         """
         Test the memoize decorator for caching and method call count.
 
-        The test class TestClass has a memoized property (a_property) that calls
+        The test class TestClass has
+        a memoized property (a_property) that calls
         a_method. This test ensures that a_property is memoized and a_method is
         called only once.
         """
